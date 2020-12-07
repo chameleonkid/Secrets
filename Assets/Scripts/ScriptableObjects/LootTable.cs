1ab6ebf15b9f31d382f2eb4c7c74beb945451bc5
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-08 00:09:54 +1100

    Change `Loottable` case (res)

---
+++ Assets/Scripts/ScriptableObjects/LootTable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Loot
+{
+    public PowerUps thisLoot;
+    public float lootChance;
+}
+
+[CreateAssetMenu]
+public class LootTable : ScriptableObject
+{
+    public Loot[] loots;
+
+    public PowerUps LootPowerUp()
+    {
+        float cumProb = 0;
+        float currentProb = Random.Range(0, 100);
+        for (int i = 0; i < loots.Length; i++)
+        {
+            cumProb += loots[i].lootChance;
+            if (currentProb <= cumProb)
+            {
+                return loots[i].thisLoot;
+            }
+        }
+        return null;
+    }
+}
