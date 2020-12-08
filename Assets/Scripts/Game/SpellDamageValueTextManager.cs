46719bc3480fb8dc7e014298c94b2fb36409a8a0
Stefan Lüke <stefan-lueke@gmx.de>
2020-12-08 16:46:04 +0100

    Added SpellDamagePanel in Inventory
    SpellDamageValue added to Inventory

---
+++ Assets/Scripts/Game/SpellDamageValueTextManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpellDamageValueTextManager : MonoBehaviour
+{
+
+    public PlayerInventory playerInventory;
+    public TextMeshProUGUI SpellDamageDisplay;
+
+    private void Start()
+    {
+        UpdateSpellDamageValue();
+    }
+
+
+    public void UpdateSpellDamageValue()
+    {
+        if (playerInventory.currentSpellbook)
+        {
+            SpellDamageDisplay.text = "" + playerInventory.currentSpellbook.SpellDamage;
+        }
+        else
+        {
+            SpellDamageDisplay.text = "0";
+        }
+    }
+
+    private void OnEnable()
+    {
+        UpdateSpellDamageValue();
+    }
+
+}
+
