8810c42767f65bc71e6f8aef1a0842b36cd4a1b6
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-10 16:06:25 +1100

    Match `IceShardSpell` classname with filename

--- Assets/Scripts/Objects/Projectiles/IceShardSpell.cs
+++ Assets/Scripts/Objects/Projectiles/IceShardSpell.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpellIceShard : PlayerProjectile
+public class IcsShardSpell : PlayerProjectile
 {
     public float slowTime;
     public Animator anim;
