6ff362e0e5e720cb57168e8eb9b061f4354330fd
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-10 13:24:19 +1100

    Remove `[RequireComponent(typeof(Rigidbody2D)]` from `Projectile`
    `PlayerProjectile` needs to be able to destroy the rigidbody

--- Assets/Scripts/Objects/Projectile.cs
+++ Assets/Scripts/Objects/Projectile.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+[RequireComponent(typeof(Collider2D))]
 public class Projectile : MonoBehaviour
 {
     [SerializeField] protected float lifetime;
