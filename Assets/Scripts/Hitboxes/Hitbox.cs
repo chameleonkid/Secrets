d9f01a00c6806dac0dc2fc862e4dc45b85f077c1
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-11 09:57:34 +1100

    Create abstract `Hitbox` class

---
+++ Assets/Scripts/Hitboxes/Hitbox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public abstract class Hitbox : MonoBehaviour
+{
+    [Tooltip("The tag this hitbox is able to affect")]
+    [SerializeField] private string targetTag = default;
+
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.isTrigger && other.CompareTag(targetTag))
+        {
+            OnHit(other);
+        }
+    }
+
+    protected abstract void OnHit(Collider2D other);
+}
