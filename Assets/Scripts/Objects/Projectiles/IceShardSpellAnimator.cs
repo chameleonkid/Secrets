c99395e24b9c98adb806595f636ecbcaf7253c35
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-11 15:25:25 +1100

    Refactor ice spell
    Added `SlowOnTrigger`

---
+++ Assets/Scripts/Objects/Projectiles/IceShardSpellAnimator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class IceShardSpellAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator= transform.GetComponent<Animator>();
+        animator.SetBool("isFlying", true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("enemy"))
+        {
+            animator.SetBool("isFlying", false);
+        }
+    }
+}
