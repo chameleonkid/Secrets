a19769f748dbc8dc585c0424d2c6a54a97d12f49
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-11 15:32:27 +1100

    Add `ColorOnTrigger`

---
+++ Assets/Scripts/Hitboxes/ColorOnTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ColorOnTrigger : Hitbox
+{
+    [SerializeField] private Color color = Color.white;
+    [SerializeField] private float duration = 2;
+
+    protected override void OnHit(Collider2D other)
+    {
+        var hit = other.GetComponent<ChangeColor>();
+        if (hit != null)
+        {
+            hit.ChangeSpriteColor(color, duration);
+        }
+    }
+}
