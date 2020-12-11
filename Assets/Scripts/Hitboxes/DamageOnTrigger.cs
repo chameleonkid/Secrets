11a9daacf6d27d3a35b20f378176d11d7ee94c74
Stefan Lüke <stefan-lueke@gmx.de>
2020-12-11 19:01:30 +0100

    Implemented DamageOnTrigger
    Some minor fixes to apply crit in DamageOnTrigger
    Still Spells dont work (Reference to Inventory needed)

--- Assets/Scripts/Hitboxes/DamageOnTrigger.cs
+++ Assets/Scripts/Hitboxes/DamageOnTrigger.cs
@@ -9,11 +9,46 @@ public class DamageOnTrigger : Hitbox
     protected override void OnHit(Collider2D other)
     {
         var hit = other.GetComponent<Character>();
+        Debug.Log(other + "was hit by "+ this );
         if (hit != null)
         {
-            var dmg = isCritical ? damage * 2 : damage;
-            hit.health -= damage;
-            DamagePopUpManager.RequestDamagePopUp(dmg, isCritical, hit.transform);
+            if (other.GetComponent<Enemy>() && this.GetComponentInParent<PlayerMovement>())
+            {
+                isCritical = this.GetComponentInParent<PlayerMovement>().IsCriticalHit();
+                damage = this.GetComponentInParent<PlayerMovement>().myInventory.currentWeapon.damage;
+                var dmg = isCritical ? damage * 2 : damage;
+                Debug.Log(this + " hit " + other + " for " + dmg + " and it was critical? " + isCritical);
+                hit.health -= dmg; //this was hit.health -= (damage);  but i dont think this made sense???
+                DamagePopUpManager.RequestDamagePopUp(dmg, isCritical, hit.transform);
+            }
+            if(other.GetComponent<PlayerMovement>())
+            {
+                var defense = other.GetComponent<PlayerMovement>().myInventory.totalDefense;
+                var dmg = isCritical ? (damage * 2) -defense : damage - defense;
+                if (dmg >= 0)
+                {
+                    Debug.Log(other + " hit " + this + " for " + dmg + " and it was critical? " + isCritical);
+                    hit.health -= (dmg); //this was hit.health -= (damage);  but i dont think this made sense???
+                    DamagePopUpManager.RequestDamagePopUp(dmg, isCritical, hit.transform);
+                }
+                else
+                {
+                    Debug.Log(other + " hit " + this + " for " + dmg + " and it was critical? " + isCritical);
+                    Debug.Log("DMG was to low to pierce armor");
+                    DamagePopUpManager.RequestDamagePopUp(0, isCritical, hit.transform);
+                }
+            }
+            if (this.gameObject.CompareTag("spell"))
+            {
+                damage = this.GetComponentInParent<PlayerMovement>().myInventory.totalSpellDamage; //how can i get a reference to the playerInventory...
+                var dmg = isCritical ? damage * 2 : damage;
+                Debug.Log(this + " hit " + other + " for " + dmg + " and it was critical? " + isCritical);
+                hit.health -= dmg; //this was hit.health -= (damage);  but i dont think this made sense???
+                DamagePopUpManager.RequestDamagePopUp(dmg, isCritical, hit.transform);
+            }
+
+
+
         }
     }
 }
