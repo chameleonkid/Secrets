171644b3ef5afc16555ab90446d419e244a47f67
Stefan Lüke <stefan-lueke@gmx.de>
2020-12-16 17:41:24 +0100

    NPC-Dialogue#1 + CleanUp
    Created a simple dialoguesystem.
    
    -Created Dialogue.cs which contains the Basic-Dialogueparameter (NPC-Name,Texts[])
    -Created DialogueManager.cs to control the Panel
    -Created DialogueTrigger to attach to potential triggers
    
    -Created DialogueManager-GameObject (made it a prefab)
    -Created a NPC-Prefab with trigger
    
    -Created a Prefabfolder "Managers" and placed Managers into it

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public GameObject dialoguePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameText.text = dialogue.npcName;
+        sentences.Clear();
+        dialoguePanel.SetActive(true);
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+   public void EndDialogue()
+    {
+        dialoguePanel.SetActive(false);
+        Debug.Log("End of conversation.");
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public bool playerInRange;
+    public GameObject NextButton;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") && playerInRange)
+        {
+            TriggerDialogue();
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(NextButton);
+        }
+
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger)
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger)
+        {
+            playerInRange = false;
+            FindObjectOfType<DialogueManager>().EndDialogue();
+        }
+    }
+
+}
