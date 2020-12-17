34d7448cbf1cfe1a8e502c199545d73b9e0f472c
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-17 19:05:46 +1100

    Add CanvasManager

---
+++ Assets/Scripts/Menus/CanvasManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public static CanvasManager Instance { get; private set; }
+
+    private GameObject activeCanvas;
+
+    private void Awake() {
+        if (Instance != null && Instance != this)
+        {
+            transform.parent = null;
+            Destroy(this.gameObject);
+        }
+        else {
+            Instance = this;
+        }
+    }
+
+    public bool IsFreeOrActive(GameObject canvasGameObject)
+    {
+        if (activeCanvas == canvasGameObject || activeCanvas == null || !activeCanvas.activeInHierarchy)
+        {
+            activeCanvas = canvasGameObject;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
