e129267d50db91e51c7b4ea00141b89cac1f136c
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-17 17:26:19 +1100

    Reorganise Assets folder

--- Assets/_Schwer/Editor/PrefabMenu.cs
+++ Assets/_Schwer/Editor/PrefabMenu.cs
@@ -5,8 +5,6 @@ namespace SchwerEditor {
     public static class PrefabMenu {
         // Reference: https://docs.unity3d.com/ScriptReference/MenuItem.html
 
-        private const string chestPrefabPath = "Assets/Prefabs/Chests/ItemChest.prefab";
-
         // Editor seems to need restarting in order to reflect changes in menu item priority.
         // Reference for built-in menu item priorities: https://blog.redbluegames.com/guide-to-extending-unity-editors-menus-b2de47a746db
 
@@ -15,7 +13,8 @@ namespace SchwerEditor {
         // Intended for `Instantiate Prefab` to be below `Create Empty`, but can't use priority `1` since `3D Object` uses that.
         
         [MenuItem("GameObject/Instantiate Prefab/Item Chest", false, 0)]
-        public static void InstantiateChestPrefab(MenuCommand command) => InstantiatePrefab(command, chestPrefabPath);
+        public static void InstantiateChestPrefab(MenuCommand command) => InstantiatePrefab(command,
+            "Assets/Prefabs/Objects/Chests/ItemChest.prefab");
 
         private static void InstantiatePrefab(MenuCommand command, string path) {
             var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
