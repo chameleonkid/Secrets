703925539912dec881c7df4e519900d4df7a11fd
Stefan Lüke <stefan-lueke@gmx.de>
2020-12-20 00:23:31 +0100

    TriggerArea
    Added ColliderTrigger.cs
    
    to check for the player entering an simple Collider-Trigger.

---
+++ Assets/Scripts/Area-Triggers/ColliderTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ColliderTrigger : MonoBehaviour
+{
+    public event EventHandler OnPlayerEnterTrigger;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Character player = other.GetComponent<PlayerMovement>();
+        if (player != null) 
+        {
+            //Its the player inside the triggerArea
+            Debug.Log("Player Enteres BossArea!");
+            OnPlayerEnterTrigger?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
