35fd7d31eae22e4a0d09889b9cca06b6a7d53d5e
Stefan Lüke <stefan-lueke@gmx.de>
2020-12-20 23:07:11 +0100

    MusicManager + AssetCleanUp
    -Added a MusicManager that can play soundtracks on loop
    -Added a MusicRequester that adds specific music depending on the area to the MusicManager
    -CleanUp of unused Assesments
    -Rearranged some Prefabs regarding Audio

---
+++ Assets/Scripts/GameManagers/MusicAndSound/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+
+public class MusicManager : MonoBehaviour
+{
+    private static event Action<AudioClip> OnMusicRequested;
+
+    public static void RequestMusic(AudioClip music) => OnMusicRequested?.Invoke(music);
+
+    private AudioSource audioSource;
+
+    private void OnEnable() => OnMusicRequested += PlayMusic;
+    private void OnDisable() => OnMusicRequested -= PlayMusic;
+
+    private void Awake() => audioSource = GetComponent<AudioSource>();
+
+    private void PlayMusic(AudioClip music)
+    {
+        audioSource.clip = music;
+        audioSource.Play();
+    }
+}
