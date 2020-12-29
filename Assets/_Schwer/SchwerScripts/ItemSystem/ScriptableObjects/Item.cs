a6f38adca6d7b09e7c3fc7f4c70b41a75d13216e
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-29 18:56:05 +1100

    Match Schwer.ItemSystem.Item with old Item

--- Assets/_Schwer/SchwerScripts/ItemSystem/ScriptableObjects/Item.cs
+++ Assets/_Schwer/SchwerScripts/ItemSystem/ScriptableObjects/Item.cs
@@ -1,17 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Schwer.ItemSystem {
     [CreateAssetMenu(menuName = "Scriptable Object/Item System/Item")]
     public class Item : ScriptableObject {
         [SerializeField] private int _id = default;
         public int id => _id;
+
         [SerializeField] private string _name = default;
         public new string name => _name;
+
         [SerializeField][TextArea] private string _description = default;
         public string description => _description;
+
+        public virtual string fullDescription => _description;
+
         [SerializeField] private Sprite _sprite = default;
         public Sprite sprite => _sprite;
-        [SerializeField] private bool _stackable = true;
-        public bool stackable => _stackable;
+
+        [SerializeField] private AudioClip _sound = default;
+        public AudioClip sound => _sound;
+
+        [SerializeField] private bool _unique = default;
+        public bool unique => _unique;
+
+        [SerializeField] private bool _usable = default;
+        public bool usable => _usable;
+
+        [SerializeField] private int _level = default;
+        public int level => _level;
+
+        [SerializeField] private UnityEvent OnUse = default;
+
+        [SerializeField] private int _buyPrice = default;
+        public int buyPrice => _buyPrice;
+
+        [SerializeField] private int _sellPrice = default;
+        public int sellPrice => _sellPrice;
+
+        public void Use() => OnUse?.Invoke();
     }
 }
