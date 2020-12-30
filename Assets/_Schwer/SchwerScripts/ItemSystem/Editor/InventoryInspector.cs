e0cead2bf56d33a238bef4379255ee9c7c6536e4
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-30 12:19:41 +1100

    Update InventoryInspector.cs

--- Assets/_Schwer/SchwerScripts/ItemSystem/Editor/InventoryInspector.cs
+++ Assets/_Schwer/SchwerScripts/ItemSystem/Editor/InventoryInspector.cs
@@ -2,33 +2,31 @@
 using UnityEngine;
 
 namespace SchwerEditor.ItemSystem {
-    using Schwer.ItemSystem;
-
-    [CustomEditor(typeof(InventorySO))]
+    [CustomEditor(typeof(Inventory))]
     public class InventoryDemo : Editor {
         private static Item item;
         private static int amount = 1;
 
         public override void OnInspectorGUI() {
-            base.OnInspectorGUI();
-            GUILayout.Space(5);
-            
-            var inventory = (InventorySO)target;
+            var inventory = (Inventory)target;
             DrawDemoControls(inventory);
+
+            GUILayout.Space(5);
+            base.OnInspectorGUI();
         }
 
-        private void DrawDemoControls(InventorySO inventory) {
+        private void DrawDemoControls(Inventory inventory) {
             EditorGUILayout.LabelField("Demo Controls", EditorStyles.boldLabel);
 
             if (GUILayout.Button("Clear Inventory")) {
-                inventory.value.Clear();
+                inventory.items.Clear();
                 Debug.Log("Cleared '" + inventory.name + "'.");
             }
 
             DrawItemControls(inventory);
         }
 
-        private void DrawItemControls(InventorySO inventory) {
+        private void DrawItemControls(Inventory inventory) {
             EditorGUILayout.BeginVertical("box");
 
             item = (Item)EditorGUILayout.ObjectField("Item", item, typeof(Item), false);
@@ -38,31 +36,20 @@ namespace SchwerEditor.ItemSystem {
             var itemName = "'" + ((item != null) ? item.name : "(Item)") + "'";
 
             EditorGUI.BeginDisabledGroup(item == null);
-            if (GUILayout.Button("Check for " + itemName)) {
-                Debug.Log(invName + " has " + inventory.value[item] + "x of " + itemName + ".");
-            }
-            if (GUILayout.Button("Check " + itemName + " >= " + amount)) {
-                if (inventory.value[item] >= amount) {
-                    Debug.Log(invName + " has " + amount + " or more of " + itemName + ".");
-                }
-                else {
-                    Debug.Log("" + invName + " has less than " + amount + " of " + itemName + ".");
-                }
-            }
             if (GUILayout.Button("Set " + itemName + " to " + amount + "x")) {
-                inventory.value[item] = amount;
+                inventory.items[item] = amount;
                 Debug.Log("Set " + invName + " " + itemName + " to " + amount + "x.");
             }
             if (GUILayout.Button("Add " + amount + "x " + itemName)) {
-                inventory.value[item] += amount;
+                inventory.items[item] += amount;
                 Debug.Log("Added " + amount + "x " + itemName + " to " + invName + ".");
             }
             if (GUILayout.Button("Subtract " + amount + "x " + itemName)) {
-                inventory.value[item] -= amount;
+                inventory.items[item] -= amount;
                 Debug.Log("Removed " + amount + "x " + itemName + " from " + invName + ".");
             }
             if (GUILayout.Button("Remove all of " + itemName)) {
-                if (inventory.value.Remove(item)) {
+                if (inventory.items.Remove(item)) {
                     Debug.Log("Removed all of " + itemName + " from " + invName + ".");
                 }
                 else {
