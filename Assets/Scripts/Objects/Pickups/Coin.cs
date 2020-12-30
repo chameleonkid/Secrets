269b7ae5009b7075217f13b8726087cb3c0273a5
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-30 14:35:10 +1100

    Refactor Coin.cs to remove signal system

--- Assets/Scripts/Objects/Pickups/Coin.cs
+++ Assets/Scripts/Objects/Pickups/Coin.cs
@@ -2,16 +2,12 @@
 
 public class Coin : PickUp
 {
-    [SerializeField] private Signals coinSignal = default;
-    public Inventory PlayerInventory;
-
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         var player = other.GetComponent<PlayerMovement>();
         if (player != null)
         {
-            PlayerInventory.coins += 1;
-            coinSignal?.Raise();
+            player.myInventory.coins++;
             Destroy(this.gameObject);
         }
     }
