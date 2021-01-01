2d1310b92f6f66b5c90cee0760ed73db4c714fcb
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-01 02:28:56 +0100

    Create DamageOnTriggerNoArmor
    This is used for enemys spells that ignore armor

---
+++ Assets/Scripts/Hitboxes/DamageOnTriggerNoArmor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DamageOnTriggerNoArmor : Hitbox
+{
+    [SerializeField] private float _damage = 1;                     // default value if not overwritten by other classes/scripts
+    public float damage { get => _damage; set => _damage = value; }
+    public bool isCritical { get; set; } = false;
+
+    protected override void OnHit(Collider2D other)
+    {
+        var hit = other.GetComponent<Character>();
+        if (hit != null)
+        {
+            var dmg = isCritical ? damage * 2 : damage;
+            hit.health -= damage; 
+        }
+    }
+}
