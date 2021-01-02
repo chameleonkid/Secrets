fbbb99bd447c40de0759dabd1483b8d4c55e31a7
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-01-02 20:56:38 +1100

    Clean up Interactable.cs

--- Assets/Scripts/Objects/Interactable.cs
+++ Assets/Scripts/Objects/Interactable.cs
@@ -1,26 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-
     public bool playerInRange;
     public Signals contextOn;
     public Signals contextOff;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -28,7 +12,6 @@ public class Interactable : MonoBehaviour
         {
             contextOn.Raise();
             playerInRange = true;
-
         }
     }
 
@@ -38,9 +21,6 @@ public class Interactable : MonoBehaviour
         {
             contextOff.Raise();
             playerInRange = false;
-
         }
     }
-
-
 }
