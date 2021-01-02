0f92b99d116eb1d1926e1708f55a52e54691e7c9
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-02 01:18:52 +0100

    Seperated Weather and TimeManager to prepare the DDOL

--- Assets/Scripts/GameManagers/WeatherManager.cs
+++ Assets/Scripts/GameManagers/WeatherManager.cs
@@ -17,19 +17,10 @@ public class WeatherManager : MonoBehaviour
     [SerializeField] private bool isSunny = false;
     [SerializeField] private bool isSnowing = false;
 
-    [SerializeField] private Light2D globalLight;
-    [SerializeField] private float globalLightInitialIntensity;
-    [SerializeField] private float normalizedTimeOfDay = 0;
-    [SerializeField] private TimeTextManager time = default;
-
-    [SerializeField] private Sprite dayTimeSprite;
-    [SerializeField] private Sprite nightTimeSprite;
-    [SerializeField] private Image timeImage;
 
 
     public void Update()
     {
-        UpdateSun();
 
         if (isRaining)
         {
@@ -58,15 +49,13 @@ public class WeatherManager : MonoBehaviour
 
     public void Awake()
     {
-        globalLight = GameObject.Find("Global Light 2D").GetComponent<Light2D>();
+
+
         rainMaker = GameObject.Find("RainMaker").GetComponent<ParticleSystem>();
         snowMaker = GameObject.Find("SnowMaker").GetComponent<ParticleSystem>();
-        time = GameObject.Find("TimeInfo").GetComponent<TimeTextManager>();
-        timeImage = GameObject.Find("TimeInfo").GetComponent<Image>();
-        
         snowMakerEmission = snowMaker.emission;
         rainMakerEmission = rainMaker.emission;
-        globalLightInitialIntensity = globalLight.intensity;
+
     }
 
     public void StartSnow()
@@ -124,39 +113,4 @@ public class WeatherManager : MonoBehaviour
         yield return new WaitForSeconds(5f);
     }
 
-
-    void UpdateSun()
-    {
-        if (SceneManager.GetActiveScene().name == "Overworld")
-        {
-            var normTime = time.normalizedTimeOfDay;
-
-            float intensityMultiplier = 1;
-            if (normTime <= 0.23f || normTime >= 0.75f)
-            {
-                intensityMultiplier = 0;
-                timeImage.sprite = nightTimeSprite;
-            }
-            else if (normTime <= 0.25f)
-            {
-                intensityMultiplier = Mathf.Clamp01((normTime - 0.23f) * (1 / 0.02f));
-                timeImage.sprite = dayTimeSprite;
-            }
-            else if (normTime >= 0.73f)
-            {
-                intensityMultiplier = Mathf.Clamp01(1 - ((normTime - 0.73f) * (1 / 0.02f)));
-                timeImage.sprite = nightTimeSprite;
-            }
-
-            globalLight.intensity = globalLightInitialIntensity * intensityMultiplier;
-        }
-        else if (SceneManager.GetActiveScene().name.Contains("Dungeon"))
-        {
-            globalLight.intensity = 0;
-        }
-        else
-        {
-            globalLight.intensity = 1;
-        }
-    }
 }
