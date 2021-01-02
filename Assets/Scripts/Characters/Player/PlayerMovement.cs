a748fe3ec9462c9403ef0768c62bc530bbc292ac
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-03 00:18:16 +0100

    Character was stuck for 3 seconds

--- Assets/Scripts/Characters/Player/PlayerMovement.cs
+++ Assets/Scripts/Characters/Player/PlayerMovement.cs
@@ -204,7 +204,7 @@ public class PlayerMovement : Character
             yield return null;
             animator.SetBool("Attacking", false);
 
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(0.3f);
 
         if (currentState != State.interact)
         {
