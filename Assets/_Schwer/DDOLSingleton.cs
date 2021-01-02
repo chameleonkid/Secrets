ed0081ae9115902fe6d3a9ae535cf5674d4f31d5
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-01-02 16:00:55 +1100

    Import singleton base classes

---
+++ Assets/_Schwer/DDOLSingleton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DDOLSingleton<T> : MonoBehaviourSingleton<T> where T : MonoBehaviour {
+    protected override void Awake() {
+        if (_Instance != null && _Instance != this) {
+            Debug.Log("Instance (" + _Instance + ") already set!");
+            Destroy(this.gameObject);
+        }
+        else {
+            _Instance = this as T;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
---
+++ Assets/_Schwer/MonoBehaviourSingleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+    protected static T _Instance;
+    public static T Instance => _Instance;
+
+    protected virtual void Awake() {
+        if (_Instance != null && _Instance != this) {
+            Debug.Log("Instance (" + _Instance + ") already set!");
+            Destroy(this.gameObject);
+        }
+        else {
+            _Instance = this as T;
+        }
+    }
+}
