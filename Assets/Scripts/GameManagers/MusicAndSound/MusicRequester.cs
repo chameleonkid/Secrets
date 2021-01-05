b20f790ab5cd3bbb62f12754fbde3efca5979102
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-05 17:27:48 +0100

    Added some more music + made the musicManager play rnd areaMusic

--- Assets/Scripts/GameManagers/MusicAndSound/MusicRequester.cs
+++ Assets/Scripts/GameManagers/MusicAndSound/MusicRequester.cs
@@ -2,14 +2,17 @@
 
 public class MusicRequester : MonoBehaviour
 {
-    [SerializeField] private AudioClip areaMusic = default;
+    [SerializeField] private AudioClip[] areaMusic = default;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         var player = other.GetComponent<PlayerMovement>();
         if (player != null)
         {
-            MusicManager.RequestMusic(areaMusic);
+            MusicManager.RequestMusic(GetMusic());
         }
     }
+
+
+    private AudioClip GetMusic() => areaMusic[Random.Range(0, areaMusic.Length)];
 }
