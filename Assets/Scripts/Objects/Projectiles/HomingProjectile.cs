aa5d1ab8fc07a7be0aa6bfc76daa3814971bb50c
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-07 12:11:54 +0100

    Only set the body to zero once for homing-pos

--- Assets/Scripts/Objects/Projectiles/HomingProjectile.cs
+++ Assets/Scripts/Objects/Projectiles/HomingProjectile.cs
@@ -11,20 +11,21 @@ public class HomingProjectile : Projectile
     void Start()
     {
         target = GameObject.FindWithTag("Player").transform;
+        rigidbody.velocity = Vector2.zero;  // Needed to stop the rigidbody working vs the transform change
     }
 
     // Update is called once per frame
     void Update()
     {
-         rigidbody.velocity = Vector2.zero;  // Needed to stop the rigidbody working vs the transform change
+       
          this.transform.position = Vector2.MoveTowards(this.transform.position, target.position, projectileSpeed * Time.deltaTime); // 3 = Speed aktuell noch Hardcoded -> Projectile müssen Ihren Speed kennen...
-
+    
   
-      // Suggested way to move the rigidBody looks strange....
-      /*
-        Vector3 direction = (target.transform.position - transform.position).normalized;
-        rigidbody.MovePosition(transform.position + direction * 5 * Time.deltaTime);
-        */
+    // Suggested way to move the rigidBody looks strange....
+    /*
+    Vector3 direction = (target.transform.position - transform.position).normalized;
+    rigidbody.MovePosition(transform.position + direction * 5 * Time.deltaTime);
+    */
 
         LifetimeCountdown();
     }
