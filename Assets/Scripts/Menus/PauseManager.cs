3960a27a9dbdea5ff02db2e3ca708e0ae1780df4
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-15 13:05:27 +0100

    Refactored LoadingManager
    Created LoadManager.cs to reduce CopyPasta

--- Assets/Scripts/Menus/PauseManager.cs
+++ Assets/Scripts/Menus/PauseManager.cs
@@ -8,12 +8,6 @@ public class PauseManager : MonoBehaviour
     public GameObject pausePanel;
     public GameObject loadPanel;
     public GameObject firstButtonPause;
-    [SerializeField] private Button saveSlot1;
-    [SerializeField] private Button saveSlot2;
-    [SerializeField] private Button saveSlot3;
-    [SerializeField] private StringValue saveSlot1Text;
-    [SerializeField] private StringValue saveSlot2Text;
-    [SerializeField] private StringValue saveSlot3Text;
 
     private bool isPaused = false;
 
@@ -30,15 +24,7 @@ public class PauseManager : MonoBehaviour
             }
         }
     }
-
-    public void ClickLoadButton()
-    {
-        loadPanel.SetActive(true);
-        saveSlot1.GetComponentInChildren<Text>().text = saveSlot1Text.RuntimeValue;
-        saveSlot2.GetComponentInChildren<Text>().text = saveSlot2Text.RuntimeValue;
-        saveSlot3.GetComponentInChildren<Text>().text = saveSlot3Text.RuntimeValue;
-    }
-
+   
     private void ChangePause()
     {
         isPaused = !isPaused;
@@ -51,38 +37,7 @@ public class PauseManager : MonoBehaviour
         SceneManager.LoadScene("StartMenu");
         Time.timeScale = 1f;
     }
-
-
-    public void LoadSlot1()
-    {
-        Load("saveSlot1");
-    }
-
-    public void LoadSlot2()
-    {
-        Load("saveSlot2");
-    }
-
-    public void LoadSlot3()
-    {
-        Load("saveSlot3");
-    }
-
-    public void Load(string loadSlot)
-    {
-        SimpleSave.Instance.Load(loadSlot);
-        loadPanel.SetActive(false);
-        Time.timeScale = 1f;
-    }
-
-    public void CancelLoad()
-    {
-        loadPanel.SetActive(false);
-        EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(firstButtonPause);
-    }
-
-
+         
     public void Save() => SimpleSave.Instance.Save("saveSlot1");
 
     public void Reset() => SimpleSave.Instance.LoadNew();
--- Assets/Scripts/Menus/StartMenu.cs
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -8,60 +8,16 @@ using UnityEngine.UI;
 public class StartMenu : MonoBehaviour
 {
     public GameObject pausePanel;
-    public GameObject loadPanel;
-    public GameObject firstButtonPause;
-
-    [SerializeField] private Button saveSlot1;
-    [SerializeField] private Button saveSlot2;
-    [SerializeField] private Button saveSlot3;
-    [SerializeField] private StringValue saveSlot1Text;
-    [SerializeField] private StringValue saveSlot2Text;
-    [SerializeField] private StringValue saveSlot3Text;
-
 
     public void NewGame()
     {
         SceneManager.LoadScene("CharacterCreation");
     }
 
-    public void ClickLoadButton()
-    {
-        loadPanel.SetActive(true);
-        saveSlot1.GetComponentInChildren<Text>().text = saveSlot1Text.RuntimeValue;
-        saveSlot2.GetComponentInChildren<Text>().text = saveSlot2Text.RuntimeValue;
-        saveSlot3.GetComponentInChildren<Text>().text = saveSlot3Text.RuntimeValue;
-    }
 
     public void ExitGame()
     {
         Application.Quit();
     }
 
-    public void LoadSlot1()
-    {
-        Load("saveSlot1");
-    }
-
-    public void LoadSlot2()
-    {
-        Load("saveSlot2");
-    }
-
-    public void LoadSlot3()
-    {
-        Load("saveSlot3");
-    }
-
-    public void CancelLoad()
-    {
-        loadPanel.SetActive(false);
-        EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(firstButtonPause);
-    }
-
-    public void Load(string loadSlot)
-    {
-        SimpleSave.Instance.Load(loadSlot);
-        loadPanel.SetActive(false);
-    }
 }
