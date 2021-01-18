2b0bbccb091e77023e1e76dd7d9befaac79c2420
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-18 15:32:01 +0100

    Created SpawnTraps
    On Entering the spawn "objects"

---
+++ Assets/Scripts/Characters/Components/SpawnTrap.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrap : MonoBehaviour
+{
+    [Header("Choose random things to spawn of this:")]
+    [SerializeField] private GameObject[] thingToSpawn;
+    [Header("How many Randoms should be spawned?:")]
+    [SerializeField] private int spawnCounter;
+    [Header("How much delay between spawns?:")]
+    [SerializeField] private float spawnDelay;
+    [Header("In which radius of the trap will be spawned?:")]
+    [SerializeField] private float spawnRadius;
+    [Header("This sound will be played when the trap will be activated:")]
+    [SerializeField] private AudioClip trapSound;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<PlayerMovement>() && other.isTrigger)
+        {
+            StartSpawning();
+        }
+    }
+
+    protected virtual void StartSpawning()
+    {
+        StartCoroutine(StartSpawningCo());
+    }
+
+
+    protected virtual IEnumerator StartSpawningCo()
+    {
+        SoundManager.RequestSound(trapSound);
+        for(int i = 0; i < spawnCounter; i++)
+        {
+            Vector2 homePos = this.transform.position;
+            var randomPosition = homePos + Random.insideUnitCircle * spawnRadius;
+            var randomSpawn = Random.Range(0, thingToSpawn.Length);
+            var spawn = Instantiate(thingToSpawn[randomSpawn], randomPosition, Quaternion.identity);  //this.transform.position needs to vary slightly for iteration
+            yield return new WaitForSeconds(spawnDelay);
+        }
+
+    }
+
+
+}
