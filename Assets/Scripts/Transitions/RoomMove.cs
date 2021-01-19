8e6e4dfeac6f517d29c09bb545371ccd36c38794
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-19 20:10:25 +0100

    Created Dungeon1 + Some doors (BossDoor/EnemyDoor)

--- Assets/Scripts/Transitions/RoomMove.cs
+++ Assets/Scripts/Transitions/RoomMove.cs
@@ -6,17 +6,13 @@ using UnityEngine.UI;
 public class RoomMove : MonoBehaviour
 {
 
-    //public Vector2 cameraChange;
-    //  public Vector2 cameraMaxChange;
-    //  public Vector2 cameraMinChange;
+
     public Vector3 playerChange;
-    // private CameraMovement cam;
     public bool needText;
     public string AreaName;
     public GameObject IGAreaText;
     public Text AreaText;
 
-    //Teleport der Kamera und des Spielers bei Areawechsel
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
---
+++ Assets/Scripts/Transitions/Teleport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teleport : MonoBehaviour
+{
+    public Vector3 playerChange;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>() && !other.isTrigger)
+        {
+            other.GetComponent<PlayerMovement>().LockMovement(2);
+            other.transform.position = playerChange;
+        }
+    }
+
+}
