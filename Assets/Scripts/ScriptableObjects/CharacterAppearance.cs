4e09e5155f8ffa2699d81eaee265ae01ff6d2191
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-01-26 01:05:23 +1100

    Update CharacterAppearance.cs
    - Store skin index for each Texture2D
    - Create nested serializable class
    - Add methods for converting between serialzable and non-serializable forms.

--- Assets/Scripts/ScriptableObjects/CharacterAppearance.cs
+++ Assets/Scripts/ScriptableObjects/CharacterAppearance.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "Scriptable Object/Appearance")]
-[System.Serializable]
 public class CharacterAppearance : ScriptableObject
 {
     public Texture2D bodyStyle;
@@ -13,4 +9,50 @@ public class CharacterAppearance : ScriptableObject
     public Texture2D armorStyle;
     public Texture2D eyeColor;
     public string playerName;
+
+    public int bodyIndex { get; set; }
+    public int hairIndex { get; set; }
+    public int armorIndex { get; set; }
+    public int eyeIndex { get; set; }
+
+    public CharacterAppearanceSerializable GetSerializable() => new CharacterAppearanceSerializable(this);
+
+    public void Deserialize(CharacterAppearanceSerializable cas, SkinTexturesDatabase skinTextures)
+    {
+        hairColor = cas.hairColor;
+        playerName = cas.playerName;
+
+        bodyIndex = cas.bodyIndex;
+        hairIndex = cas.hairIndex;
+        armorIndex = cas.armorIndex;
+        eyeIndex = cas.eyeIndex;
+
+        bodyStyle = skinTextures.bodySkins[bodyIndex];
+        hairStyle = skinTextures.hairStyles[hairIndex];
+        armorStyle = skinTextures.armorSkins[armorIndex];
+        eyeColor = skinTextures.eyeSkins[eyeIndex];
+    }
+
+    [System.Serializable]
+    public struct CharacterAppearanceSerializable
+    {
+        public Color hairColor { get; private set; }
+        public string playerName { get; private set; }
+
+        public int bodyIndex { get; private set; }
+        public int hairIndex { get; private set; }
+        public int armorIndex { get; private set; }
+        public int eyeIndex { get; private set; }
+
+        public CharacterAppearanceSerializable(CharacterAppearance ca)
+        {
+            hairColor = ca.hairColor;
+            playerName = ca.playerName;
+
+            bodyIndex = ca.bodyIndex;
+            hairIndex = ca.hairIndex;
+            armorIndex = ca.armorIndex;
+            eyeIndex = ca.eyeIndex;
+        }
+    }
 }
