3d95babe9f06e1cb038ffaa0883784f8bbf14042
Stefan Lüke <stefan-lueke@gmx.de>
2021-01-25 16:26:23 +0100

    Created an AreaNameManager

---
+++ Assets/Scripts/GameManagers/AreaNameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class AreaNameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro areaNameDisplay;
+    [SerializeField] private string areaNameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowAreaNameCo());
+    }
+
+    IEnumerator ShowAreaNameCo()
+    {
+        this.gameObject.SetActive(true);
+        areaNameDisplay.text = areaNameText;
+        yield return new WaitForSeconds(3);
+        this.gameObject.SetActive(false);
+
+    }
+}
