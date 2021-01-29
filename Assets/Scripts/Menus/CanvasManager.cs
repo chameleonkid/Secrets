230a719f3526032c83bacb4b4fc240e317f28e39
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-01-29 20:47:58 +1100

    Prevent canvases from closing and opening in the same frame

--- Assets/Scripts/Menus/CanvasManager.cs
+++ Assets/Scripts/Menus/CanvasManager.cs
@@ -5,10 +5,15 @@ public class CanvasManager : MonoBehaviourSingleton<CanvasManager>
 {
     private GameObject activeCanvas;
 
+    private GameObject previousCanvas;
+    private int previousCanvasFrame;
+
     public bool IsFreeOrActive(GameObject canvasGameObject)
     {
-        if (activeCanvas == canvasGameObject || activeCanvas == null || !activeCanvas.activeInHierarchy)
+        if (IsSameOrCanReplace(canvasGameObject) && NotReplacingSameFrame(canvasGameObject))
         {
+            previousCanvasFrame = Time.frameCount;
+            previousCanvas = activeCanvas;
             activeCanvas = canvasGameObject;
             return true;
         }
@@ -17,4 +22,7 @@ public class CanvasManager : MonoBehaviourSingleton<CanvasManager>
             return false;
         }
     }
+
+    private bool IsSameOrCanReplace(GameObject newCanvas) => activeCanvas == newCanvas || activeCanvas == null || !activeCanvas.activeInHierarchy;
+    private bool NotReplacingSameFrame(GameObject newCanvas) => previousCanvas == null || !(previousCanvas == newCanvas && Time.frameCount == previousCanvasFrame);
 }
