b518ab940dc0c6e62e1affd719613e602ddd6dfe
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-02-11 02:28:27 +1100

    Rearrange CharacterAppearance.cs for consistency

--- Assets/Scripts/ScriptableObjects/CharacterAppearance.cs
+++ Assets/Scripts/ScriptableObjects/CharacterAppearance.cs
@@ -5,14 +5,16 @@ public class CharacterAppearance : ScriptableObject
 {
     public Texture2D bodyStyle;
     public Texture2D hairStyle;
-    public Color hairColor;
     public Texture2D armorStyle;
     public Texture2D eyeColor;
-    public string playerName;
+
+    public string playerName;   // Should probably make into property with private backing field exposed to Inspector
 
     public int index { get; set; }
+
     public int bodyIndex { get; set; }
     public int hairIndex { get; set; }
+    public Color hairColor;     // Should probably make into property with private backing field exposed to Inspector
     public int armorIndex { get; set; }
     public int eyeIndex { get; set; }
 
@@ -26,12 +28,12 @@ public class CharacterAppearance : ScriptableObject
 
     public void Deserialize(CharacterAppearanceSerializable cas, SkinTexturesDatabase[] skinTextures)
     {
-        hairColor = cas.hairColor;
         playerName = cas.playerName;
         index = cas.index;
 
         bodyIndex = cas.bodyIndex;
         hairIndex = cas.hairIndex;
+        hairColor = cas.hairColor;
         armorIndex = cas.armorIndex;
         eyeIndex = cas.eyeIndex;
 
@@ -51,26 +53,27 @@ public class CharacterAppearance : ScriptableObject
     [System.Serializable]
     public struct CharacterAppearanceSerializable
     {
-        [ES3Serializable] public int index { get; private set; }
-
-        [ES3Serializable] public Color hairColor { get; private set; }
         [ES3Serializable] public string playerName { get; private set; }
+
+        [ES3Serializable] public int index { get; private set; }
 
         [ES3Serializable] public int bodyIndex { get; private set; }
         [ES3Serializable] public int hairIndex { get; private set; }
+        [ES3Serializable] public Color hairColor { get; private set; }
         [ES3Serializable] public int armorIndex { get; private set; }
         [ES3Serializable] public int eyeIndex { get; private set; }
 
         public CharacterAppearanceSerializable(CharacterAppearance ca)
         {
-            hairColor = ca.hairColor;
             playerName = ca.playerName;
+
+            index = ca.index;
 
             bodyIndex = ca.bodyIndex;
             hairIndex = ca.hairIndex;
+            hairColor = ca.hairColor;
             armorIndex = ca.armorIndex;
             eyeIndex = ca.eyeIndex;
-            index = ca.index;
         }
     }
 }
