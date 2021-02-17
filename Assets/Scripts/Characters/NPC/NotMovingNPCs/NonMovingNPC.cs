2047590ab867e9d9217dd4ade46944c15010eb0d
Stefan Lüke <stefan-lueke@gmx.de>
2021-02-17 22:44:19 +0100

    Created Non-Moving-NPCs

---
+++ Assets/Scripts/Characters/NPC/NotMovingNPCs/NonMovingNPC.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Interactable))]
+public class NonMovingNPC : Character
+{
+    public override float health { get => 1; set { } }   //! Temp
+
+    private Interactable interactable;
+    [SerializeField] private bool lookUp;
+    [SerializeField] private bool lookDown;
+    [SerializeField] private bool lookLeft;
+    [SerializeField] private bool lookRight;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        interactable = GetComponent<Interactable>();
+        if(lookUp)
+        {
+            animator.SetFloat("moveY", 1);
+        }
+        if (lookDown)
+        {
+            animator.SetFloat("moveY", -1);
+        }
+        if (lookLeft)
+        {
+            animator.SetFloat("moveX", -1);
+        }
+        if (lookRight)
+        {
+            animator.SetFloat("moveX", 1);
+        }
+    }
+}
