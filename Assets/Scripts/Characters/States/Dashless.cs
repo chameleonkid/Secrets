d586a1c805a8ffa3bdf3f3d78464cfccc5515b07
Stefan Lüke <stefan-lueke@gmx.de>
2021-02-18 17:35:59 +0100

    Status Dashless was created

---
+++ Assets/Scripts/Characters/States/Dashless.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Dashless : Status
+{
+    [SerializeField] private float duration = 4;
+    [SerializeField] private ParticleSystem particles = default;
+
+    private float timer;
+
+    private IDashless target;
+
+    public void Initialise(IDashless target) => this.target = target;
+
+    public override void Trigger()
+    {
+        if (target == null) return;
+
+        target.canDash = false;
+        timer = duration;
+
+        var e = particles.emission;
+        e.enabled = true;
+
+        this.enabled = true;
+    }
+
+    public override void Clear() => Clear(false);
+    private void Clear(bool forceClear)
+    {
+        if (target == null) return;
+
+        if (timer > 0 || forceClear)
+        {
+            target.canDash = true;
+            timer = 0;
+        }
+
+        var e = particles.emission;
+        e.enabled = false;
+
+        this.enabled = false;
+    }
+
+    protected override void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Clear(true);
+        }
+    }
+}
+
+public interface IDashless
+{
+    Dashless dashless { get; }
+    bool canDash { get; set; }
+}
---
+++ Assets/Scripts/Characters/States/Triggers/DashLessOnTrigger.cs
@@ -0,0 +1,7 @@
+﻿public class DashLessOnTrigger : StatusOnTrigger<IDashless>
+{
+    protected override void Trigger(IDashless target)
+    {
+        target.dashless.Trigger();
+    }
+}
