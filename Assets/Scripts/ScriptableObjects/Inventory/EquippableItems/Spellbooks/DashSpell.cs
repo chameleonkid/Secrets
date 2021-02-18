bff24c579069746a1f6aeaed9093b22642c5889e
Stefan Lüke <stefan-lueke@gmx.de>
2021-02-18 15:26:33 +0100

    Created a Dashspell

---
+++ Assets/Scripts/ScriptableObjects/Inventory/EquippableItems/Spellbooks/DashSpell.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Scriptable Object/Items/Spellbook/DashSpell")]
+public class DashSpell : InventorySpellbook {
+
+    private Collider2D characterCollider;
+
+public void Dash(Character character)
+    {
+
+        characterCollider = character.GetComponent<Collider2D>();
+        Vector2 facingDirection = new Vector2(character.animator.GetFloat("moveX"), character.animator.GetFloat("moveY"));
+        Vector2 temp;
+        temp.x = facingDirection.x * character.maxDashDistance;
+        temp.y = facingDirection.y * character.maxDashDistance;
+        Vector2 dashTarget = (Vector2)character.transform.position + temp;
+        Debug.Log("Player is at: " + character.transform.position);
+        Debug.Log("DashTarget it at: " + dashTarget);
+        if (character.canDash)
+        {
+            Debug.Log("Dashing to " + dashTarget);
+            character.Dash(character, temp);
+          //  character.TeleportTowards(dashTarget, character.maxDashDistance * character.speedModifier);
+        }
+    }
+
+
+
+}
