d96998c7762200c0cc693dd437106655cc3be0c2
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-02-19 12:50:01 +1100

    Create State base class

---
+++ Assets/Scripts/Characters/States/State.cs
@@ -0,0 +1,7 @@
+﻿public abstract class State
+{
+    public abstract void Enter();
+    public virtual void Update() {}
+    public virtual void FixedUpdate() {}
+    public abstract void Exit();
+}
