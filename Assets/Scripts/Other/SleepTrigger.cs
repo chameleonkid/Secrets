d83e4bc4192851d82e1d008faae2f8bfcdefb2c9
Stefan Lüke <stefan-lueke@gmx.de>
2021-03-22 10:24:22 +0100

    Added a Sleeptrigger which sets a new TimeOfDay

---
+++ Assets/Scripts/Other/SleepTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepTrigger : MonoBehaviour
+{
+
+    [SerializeField] private FloatValue normalizedTimeOfDay = default;
+    [SerializeField] private ConstrainedFloat playerHealth = default;
+    [SerializeField] private ConstrainedFloat playerMana = default;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        normalizedTimeOfDay.value = 0.25f;
+        playerHealth.current = playerHealth.max;
+        playerMana.current = playerMana.max;
+    }
+}
