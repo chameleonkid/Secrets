2d887fd5e1dc47602c4c6c71c3a3cef9b61b644f
Stefan Lüke <stefan-lueke@gmx.de>
2021-03-24 08:36:07 +0100

    Created a basic healer NPC

---
+++ Assets/Scripts/Characters/NPC/Healer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healer : MonoBehaviour
+{
+    private bool playerInRange;
+    [SerializeField] private ConstrainedFloat playerHealth;
+    [SerializeField] private ConstrainedFloat playerMana;
+
+    private void Update()
+        {
+            if (Input.GetButtonDown("Interact") && playerInRange && Time.timeScale > 0)
+            {
+                HealPlayer();
+            }
+        }
+
+    public void HealPlayer()
+    {
+        playerHealth.current = playerHealth.max;
+        playerMana.current = playerMana.max;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger)
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger)
+        {
+            playerInRange = false;
+        }
+    }
+
+}
