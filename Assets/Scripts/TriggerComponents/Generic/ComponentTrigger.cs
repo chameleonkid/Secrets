76dc8de3f03aa4733f30ed99e310d11e324ad6c7
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-03-27 13:50:17 +1100

    Create ComponentTrigger.cs & EventComponentTrigger.cs

---
+++ Assets/Scripts/TriggerComponents/Generic/ComponentTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public abstract class ComponentTrigger<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        var component = other.GetComponent<T>();
+        if (component != null)
+        {
+            OnTriggerEnter2D(component);
+        }
+    }
+
+    protected void OnTriggerExit2D(Collider2D other)
+    {
+        var component = other.GetComponent<T>();
+        if (component != null)
+        {
+            OnTriggerExit2D(component);
+        }
+    }
+
+    protected virtual void OnTriggerEnter2D(T component) {}
+    protected virtual void OnTriggerExit2D(T component) {}
+}
---
+++ Assets/Scripts/TriggerComponents/Generic/EventComponentTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class EventComponentTrigger<T> : ComponentTrigger<T> where T : MonoBehaviour
+{
+    public event Action OnTriggerEnter;
+    public event Action OnTriggerExit;
+
+    protected override void OnTriggerEnter2D(T component) => OnTriggerEnter?.Invoke();
+    protected override void OnTriggerExit2D(T component) => OnTriggerExit?.Invoke();
+}
