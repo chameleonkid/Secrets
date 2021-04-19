8027cabca2c6282d2c4546f0fd102b685a153374
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-04-19 20:19:19 +1000

    Replace EventTrigger with new HoldableButton

---
+++ Assets/Scripts/Other/HoldableButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class HoldableButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{   // Adapted from: https://stackoverflow.com/questions/55448551/unity3d-how-to-detect-when-a-button-is-being-held-down-and-released
+    private bool held;
+
+    private Button button;
+
+    private void Awake() => button = GetComponent<Button>();
+
+    private void Update()
+    {
+        if (!held) return;
+        if (!button.IsInteractable()) held = false;
+
+        button.onClick.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (button.IsInteractable())
+        {
+            held = true;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (button.IsInteractable())
+        {
+            held = false;
+        }
+    }
+}
