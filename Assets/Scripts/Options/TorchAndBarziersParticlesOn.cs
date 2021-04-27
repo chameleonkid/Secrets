2be8272afb34bbfcfc6ef1887fa3acc92e2349df
Stefan Lüke <stefan-lueke@gmx.de>
2021-04-27 09:09:02 +0200

    Created the option to disable particlesystems on torches and braziers
    OptionButton needs to be created

---
+++ Assets/Scripts/Options/TorchAndBarziersParticlesOn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchAndBarziersParticlesOn : MonoBehaviour
+{
+    [SerializeField] private BoolValue optionTorchAndBrazier;
+    [SerializeField] private GameObject fireParticleSystem;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(optionTorchAndBrazier.RuntimeValue == true)
+        {
+            fireParticleSystem.SetActive(true);
+        }
+        else
+        {
+            fireParticleSystem.SetActive(false);
+        }
+    }
+}
