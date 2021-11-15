eb5d002001d60c971d33c444f326ddc577acd527
StefanLueke <70270710+chameleonkid@users.noreply.github.com>
2021-11-15 20:50:01 +0100

    Added a bed to sleep in

--- Assets/Scripts/Other/SleepTrigger.cs
+++ Assets/Scripts/Other/SleepTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SleepTrigger : MonoBehaviour
 {
@@ -8,11 +9,44 @@ public class SleepTrigger : MonoBehaviour
     [SerializeField] private FloatValue normalizedTimeOfDay = default;
     [SerializeField] private ConstrainedFloat playerHealth = default;
     [SerializeField] private ConstrainedFloat playerMana = default;
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] private Animator anim = default;
+    [SerializeField] private PlayerMovement player = default;
+    [SerializeField] private Vector3 newPlayerPos = default;
+
+    public void Awake()
     {
-        normalizedTimeOfDay.value = 0.25f;
-        playerHealth.current = playerHealth.max;
-        playerMana.current = playerMana.max;
+        player = FindObjectOfType<PlayerMovement>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+        
+        if (other.GetComponentInChildren<PlayerMovement>())
+        {
+            normalizedTimeOfDay.value = 0.25f;
+            playerHealth.current = playerHealth.max;
+            playerMana.current = playerMana.max;
+            StartCoroutine(StartSleeping());
+      
+        }
+
+
+
+    }
+
+    private IEnumerator StartSleeping()
+    {
+        anim.SetTrigger("StartSleeping");
+        yield return new WaitForSeconds(1f);
+        newPlayerPos = player.transform.position;
+        newPlayerPos.x -= 0.5f;
+        player.transform.position = newPlayerPos;
+        anim.SetTrigger("StopSleeping");
+
     }
 
 }
+
+
