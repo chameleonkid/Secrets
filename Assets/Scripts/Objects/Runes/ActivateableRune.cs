e6b89b61db332add3d78ff01bcb3ef76f27937e5
StefanLueke <70270710+chameleonkid@users.noreply.github.com>
2021-11-27 23:24:37 +0100

    Update ActivateableRune.cs
    Runes now might need an item to be activated

--- Assets/Scripts/Objects/Runes/ActivateableRune.cs
+++ Assets/Scripts/Objects/Runes/ActivateableRune.cs
@@ -8,6 +8,9 @@ public class ActivateableRune : MonoBehaviour
     [SerializeField] private BoolValue runeActive;
     [SerializeField] private AudioClip runeSound;
     [SerializeField] private Material runeMaterial;
+    [SerializeField] private Inventory inventory;
+    [Header("This item will be needed")]
+    [SerializeField] private  Item activateItem;
     [ColorUsageAttribute(true, true)] public Color glowColorActive;
     [ColorUsageAttribute(true, true)] public Color glowColorNotActive;
 
@@ -28,8 +31,12 @@ public class ActivateableRune : MonoBehaviour
     {
         if(!activated && other.GetComponent<PlayerMovement>())
         {
-            SoundManager.RequestSound(runeSound); // Only play the sound when you walk on it and it wasnt active yet!
-            ActivateRune();
+            if(activateItem == null || inventory.Contains(activateItem))
+            {
+                SoundManager.RequestSound(runeSound); // Only play the sound when you walk on it and it wasnt active yet!
+                ActivateRune();
+            }
+
         }
 
     }
