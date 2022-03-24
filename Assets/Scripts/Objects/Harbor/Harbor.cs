934a3a8f3a2e32bc6124e6366183e10e08b03daa
Schwer <42115696+itsschwer@users.noreply.github.com>
2022-03-24 22:29:05 +1100

    Implement ship-harbor interact

--- Assets/Scripts/Objects/Harbor/Harbor.cs
+++ Assets/Scripts/Objects/Harbor/Harbor.cs
@@ -6,7 +6,6 @@ public class Harbor : ComponentTrigger<ShipMovement>
     protected override bool? needOtherIsTrigger => true;
 
     [SerializeField] private AudioClip harborSound;
-    [SerializeField] private ShipMovement myShip;
     [SerializeField] private VectorValue playerPos;
 
     [Header("This is the World that will be loaded")]
@@ -14,9 +13,20 @@ public class Harbor : ComponentTrigger<ShipMovement>
     [Header("This is the position the player will be")]
     [SerializeField] private Vector2 harborPlayerPos;
 
-    protected override void OnEnter(ShipMovement other)
+    private ShipMovement ship;
+
+    private void Update()
     {
-        myShip = other;
+        if (ship != null && ship.inputInteract)
+        {
+            playerPos.value = harborPlayerPos;
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    protected override void OnEnter(ShipMovement ship)
+    {
+        this.ship = ship;
 
         Debug.Log("isInHarborRange");
         if (harborSound)
@@ -24,9 +34,10 @@ public class Harbor : ComponentTrigger<ShipMovement>
             SoundManager.RequestSound(harborSound); //When you enter the area to land, the bell rings
             Debug.Log("ENTER HARBOR");
         }
+    }
 
-        // I want this to happen on Shipmovement Interact
-        playerPos.value = harborPlayerPos;
-        SceneManager.LoadScene(sceneToLoad);
+    protected override void OnExit(ShipMovement ship)
+    {
+        this.ship = null;
     }
 }
