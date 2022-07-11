7889b3813d2cf14b8d7eac2cc2f7d8cd3b7ad729
Schwer <42115696+itsschwer@users.noreply.github.com>
2022-07-11 21:03:39 +1000

    Update Dialogue.cs

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 
 [System.Serializable]
 public class Dialogue
 {
-    public string npcName;
+    [SerializeField] private Speaker[] _speakers;
+    public Speaker[] speakers => _speakers;
 
     [TextArea(3, 10)]
     public string[] sentences = new string[1];
+
+    [System.Serializable]
+    public class Speaker
+    {
+        [SerializeField] private string _name;
+        public string name => _name;
+        [SerializeField] private Color _textboxColor;
+        public Color textboxColor => _textboxColor;
+    }
 }
