a273deef86283f72c0b7f3c31b969cbaf6f66cb2
StefanLueke <70270710+chameleonkid@users.noreply.github.com>
2022-10-18 12:32:13 +0200

    Creating Wolvepacks - also a player-follow script

---
+++ Assets/Scripts/Other/FollowPlayer.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FollowPlayer : Character
+{
+    //place this script on the player gameobject
+    public override float health { get => 1; set { } }   //! Temp
+
+    public GameObject player; // in the inspector drag the gameobject the will be following the player to this field
+    public int followDistance;
+    private List<Vector3> storedPositions;
+    private Vector3 directionVector;
+    private Interactable interactable;
+    [SerializeField] private float followOffSetX;
+    [SerializeField] private float followOffSetY;
+    [SerializeField] private float FollowDelaySecondsMin=0;
+    [SerializeField] private float FollowDelaySecondsMax=5;
+
+
+    void Awake()
+    {
+        base.Awake();
+        storedPositions = new List<Vector3>(); //create a blank list
+
+        if (!player)
+        {
+            Debug.Log("The FollowingMe gameobject was not set");
+        }
+
+        if (followDistance == 0)
+        {
+            Debug.Log("Please set distance higher then 0");
+        }
+    }
+    void Update()
+    {
+        if (storedPositions.Count == 0)
+        {
+            Debug.Log("blank list");
+            storedPositions.Add(player.transform.position); //store the players currect position
+            return;
+        }
+        else if (storedPositions[storedPositions.Count - 1] != player.transform.position)
+        {
+            //Debug.Log("Add to list");  
+            storedPositions.Add(player.transform.position); //store the position every frame
+        }
+
+        if (storedPositions.Count > followDistance)
+        {
+            StartCoroutine(FollowCO());
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+    private IEnumerator FollowCO()
+    {
+        var followDelayInSeconds = Random.Range(FollowDelaySecondsMin, FollowDelaySecondsMax);
+        yield return new WaitForSeconds(followDelayInSeconds);
+        Vector3 position = new Vector3(storedPositions[0].x + followOffSetX, storedPositions[0].y + followOffSetY, storedPositions[0].z);
+        transform.position = position; //move
+        animator.SetBool("isMoving", true);
+        storedPositions.RemoveAt(0); //delete the position that player just moved to
+    }
+}
