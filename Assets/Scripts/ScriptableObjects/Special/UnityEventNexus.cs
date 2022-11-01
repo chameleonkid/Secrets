27e556e58d9f6c649aaa7dc64fb12019d8e724ee
StefanLueke <70270710+chameleonkid@users.noreply.github.com>
2022-11-01 20:56:27 +0100

    A new green Potion
    it will shrink or de-shrink you!

--- Assets/Scripts/ScriptableObjects/Special/UnityEventNexus.cs
+++ Assets/Scripts/ScriptableObjects/Special/UnityEventNexus.cs
@@ -11,4 +11,18 @@ public class UnityEventNexus : ScriptableObject
 
     [SerializeField] private ConstrainedFloat lumen = default;
     public void ModifyCurrentLumen(float deltaLumen) => lumen.current += (lumen.max/100)*deltaLumen;
+    
+    public void ShrinkSize()
+    {
+        var player = FindObjectOfType<PlayerMovement>();
+        if (player.transform.localScale.x < 1 && player.transform.localScale.y < 1)
+        {
+            player.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            player.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+    }
+
 }
