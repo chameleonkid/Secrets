26d97e5e97f28056ceca80be682045c44bcde190
StefanLueke <70270710+chameleonkid@users.noreply.github.com>
2022-11-01 20:37:27 +0100

    Potions now give heal,mana and lumen percent based

--- Assets/Scripts/ScriptableObjects/Special/UnityEventNexus.cs
+++ Assets/Scripts/ScriptableObjects/Special/UnityEventNexus.cs
@@ -4,11 +4,11 @@
 public class UnityEventNexus : ScriptableObject
 {
     [SerializeField] private ConstrainedFloat mana = default;
-    public void ModifyCurrentMana(float deltaMana) => mana.current += deltaMana;
+    public void ModifyCurrentMana(float deltaMana) => mana.current += (mana.max/100)*deltaMana;
 
     [SerializeField] private ConstrainedFloat health = default;
-    public void ModifyCurrentHealth(float deltaHealth) => health.current += deltaHealth;
+    public void ModifyCurrentHealth(float deltaHealth) => health.current += (health.max/100)*deltaHealth;
 
     [SerializeField] private ConstrainedFloat lumen = default;
-    public void ModifyCurrentLumen(float deltaLumen) => lumen.current += deltaLumen;
+    public void ModifyCurrentLumen(float deltaLumen) => lumen.current += (lumen.max/100)*deltaLumen;
 }
